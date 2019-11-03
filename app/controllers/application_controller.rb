552bb9980ecd3b08a098dab1f5105a7723d64696
Randel Jiao <randeljiao@gmail.com>
2019-11-03 18:15:03 +0000

    Moving everything

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,73 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "coffeeprofiles"
   end
 
   get "/" do
     erb :welcome
   end
+
+  get '/posts' do
+      if !logged_in?
+      erb :login
+    else 
+      erb :posts
+    end
+  end
+  
+  post '/posts' do
+   erb :display
+  end
+
+  get '/edit' do
+    if !logged_in?
+       erb :login
+    else
+      post = Post.find(params[:user_id])
+      if post.user_id == current_user.id
+      "An edit post"
+    end
+  end
+  end
+  
+
+  get '/sessions/posts'do
+    if !logged_in?
+      erb :login
+    else
+      erb :posts
+    end
+ end 
+ 
+  get '/logout' do
+    erb :welcome
+  end
+  
+  helpers do
+    def logged_in?
+      !!@current_user
+    end
+    
+    def login(username, password)
+       user = User.find_by(:username => username)
+       if user  && user.authenticate(password)
+         session[:username] = user.username
+       else
+         erb :login
+       end 
+     end
+     
+     def logout!
+       session.clear
+     end
+     
+    def current_user
+      @current_user ||  User.find_by(:username => session[:username]) if session[:username]
+      #User.find(session[:username])
+    end
+  
+ end 
 
 end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,32 @@
+class UserController < ApplicationController
+  
+   get '/signup' do
+    erb :signup
+  end
+  
+  post '/signup' do
+    @user = User.new
+    @user.username = params[:username]
+    @user.password = params[:password]
+    if @user.save
+      erb :posts
+    else
+      erb :signup
+    end
+  end
+  
+  get '/login' do
+    erb :login
+  end
+  
+  post '/login' do 
+     login(params[:username], params[:password])
+     erb :posts
+   end
+  
+  
+ post '/sessions' do
+    login(params[:username], params[:password])
+    erb :posts
+  end
+end
