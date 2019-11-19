74dbca0af5a0f165a52bc5bf01e59ae9993847d1
Randel Jiao <randeljiao@gmail.com>
2019-11-19 20:19:15 +0000

    Validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user 
 
+validates :name, :prepare, :region, presence: true
 
+ 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
  has_secure_password
  
  has_many :posts
+ validates :username, uniqueness: true
+ validates :email, uniqueness: true
 end
